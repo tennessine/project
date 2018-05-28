cb106019b61d07589eeba4cf041333df0091cf1a
gekongfei <admin@w3hacker.com>
2018-05-28 18:23:58 +0800

    公司信息

--- src/main.js
+++ src/main.js
@@ -7,7 +7,21 @@ import router from './router'
 import 'amfe-flexible'
 import 'normalize.css'
 
+import VueAMap from 'vue-amap'
+
 Vue.config.productionTip = false
+
+Vue.use(VueAMap)
+
+// 初始化vue-amap
+VueAMap.initAMapApiLoader({
+  // 高德的key
+  key: '7303358a602abd6183681983c729318d',
+  // 插件集合
+  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor'],
+  // 高德 sdk 版本，默认为 1.4.4
+  v: '1.4.4'
+})
 
 /* eslint-disable no-new */
 new Vue({
